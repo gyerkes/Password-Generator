018cd63adc75aed7b9e511f0f9f1fb346f339aab
Greg Yerkes <gyerkes@rajant.com>
2023-10-05 22:54:42 -0400

    made changes to code and added some more addition comments

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
  // Assignment Code
 var generateBtn = document.querySelector("#generate");
-// Empty Array To be Filled With Characters The User Choses
-var userChoiceArray = [];
+
+
 
 // Arrays With All Valid Characters To Be Used In Password
 var lowerCaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
@@ -9,19 +9,22 @@ var upperCaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"
 var numsArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialCharactersArray = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "[", "{", "]", "}", "|", ";", ":", "'", ",", "<", ".", ">", "/", "?"];
 
+// Empty Array To be Filled With Characters The User Choses
+var userArray = [];
+
 // For Loop To Generate Passord
 function generatePassword() {
   var password = "";
   for (var i = 0; i < passwordLength; i++) {
-    var randomChoice = Math.floor(Math.random() * userChoiceArray.length);
-    password = password + userChoiceArray[randomChoice]
+    var randomChoice = Math.floor(Math.random() * userArray.length);
+    password = password + userArray[randomChoice]
   }
   return password;
 }
 
 // Screen Prompts For Generating The Password
 function screenPrompts() {
-  userChoiceArray = [];
+  userArray = [];
 
   passwordLength = parseInt(prompt("How Many Characters Do You Want Your Password To Be? Needs To Be Between 8 - 128 Characters"))
 
@@ -30,29 +33,29 @@ function screenPrompts() {
     return false;
   }
    if (confirm("Would You Like Lowercase Letters In Your Password?")) {
-   userChoiceArray = userChoiceArray.concat(lowerCaseArray)
+   userArray = userArray.concat(lowerCaseArray);
    }
    if (confirm("Would You Like Uppercase Letters In Your Password?")) {
-    userChoiceArray = userChoiceArray.concat(upperCaseArray)
+    userArray = userArray.concat(upperCaseArray);
   }
   if (confirm("Would You Like Numbers Letters In Your Password?")) {
-    userChoiceArray = userChoiceArray.concat(numsArray)
+    userArray = userArray.concat(numsArray);
   }
   if (confirm("Would You Like Special Characters Letters In Your Password?")) {
-    userChoiceArray = userChoiceArray.concat(specialCharactersArray)
+    userArray = userArray.concat(specialCharactersArray);
   }
   return true;
 }
 
-// Write password to the #password input
-// Added a Variable If Statement To The writePassword Function To Write Password Based On The User Selected Prompts
-function writePassword() {
-  var completePrompts = screenPrompts();
 
-  if (completePrompts) {
+// Write password to the #password input
+// Added a Variable to the writePassword function to assign screenprompts "true" to completed prompts 
+function writePassword() {
+  var completedPrompts = screenPrompts();
+// added if statement for if prompts are completed assign the variables. 
+  if (completedPrompts) {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
 
   passwordText.value = password;
   }
